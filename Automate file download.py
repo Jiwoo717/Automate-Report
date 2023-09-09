ea26edad4f841c47f9983c7925e97eb0bc786e07
Jiwoo717 <kimeric717@gmail.com>
2023-09-08 23:17:08 -0700

    updating my code

--- Automate file download.py
+++ Automate file download.py
@@ -3,15 +3,16 @@ from urllib3 import request
 import datetime
 
 driver = webdriver.Chrome(executable_path='../drivers/chromedriver')
-#url launch
+# url launch
 driver.get("downloadlink_URL")
-#identify link with partial link text
-l = driver.find_elements_by_partial_link_text('CASummaryReport'+ datetime.date.today().strftime('%Y%m_%d') + '_1101.xlsx')
+# identify link with partial link text
+l = driver.find_elements_by_partial_link_text(
+    'ExampleSummaryReport' + datetime.date.today().strftime('%Y%m_%d'))
 
-#perform click
+# perform click
 l.click()
 print('Page navigated after click: ' + driver.title)
-#driver quit
+# driver quit
 driver.quit()
 
 URL = "URL_of_choice(hidden_for_privacy)"
